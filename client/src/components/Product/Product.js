7dd9fe6f2fa7feaa07bea8500a155a8369dd5940
Yeison <enmanuelquiterios@gmail.com>
2022-02-05 12:26:57 -0500

    Adding Products styles

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButtom,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+import useStyles from "./styles";
 
 const Product = ({ product }) => {
-  return <div></div>;
+  const { name } = product;
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="" title={name}></CardMedia>
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom></Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Product;
