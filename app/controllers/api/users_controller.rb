9a0d9ce66a057d14eaed475300bee1e9898852a1
Yeison <enmanuelquiterios@gmail.com>
2022-02-05 11:49:26 -0500

    Added React dependancies

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::UsersController < ApplicationController
+  
+    
+
+
+
+
+
+
+
+
+
+    private 
+
+    def user_params
+        params.permit(:username, :email, :password, :password_confirmation)
+    end
 end
