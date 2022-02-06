6c58152ebbce528bdc188f6ef4e3f667a9e0b77d
Yeison <enmanuelquiterios@gmail.com>
2022-02-06 09:38:31 -0500

    Created my three main components to handle steps of the payment process

--- client/src/components/Checkout/Checkout.js
+++ client/src/components/Checkout/Checkout.js
@@ -10,11 +10,25 @@ import {
   Typography,
 } from "@material-ui/core";
 import useStyles from "./styles";
+import PaymentForm from "./CheckoutForms/PaymentForm";
+import AddressForm from "./CheckoutForms/AddressForm";
 
 const Checkout = () => {
+  // Notes: Confirmation can only be seen when activeSteps = 2
   const [activeSteps, setActiveSteps] = useState(0);
+
+  // Here I steps.lenght to render confirmation form
   const steps = ["Shipping Address", "Payment details"];
   const classes = useStyles();
+
+  // Functional components to return base on our current steps
+
+  const Form = () => (activeSteps === 0 ? <AddressForm /> : <PaymentForm />);
+
+  // Will use confirmation form when the length of activeSteps === steps.length
+
+  const Confirmation = () => <h1>Confirmation</h1>;
+
   return (
     <>
       <div className={classes.toolbar} />
@@ -30,6 +44,8 @@ const Checkout = () => {
               </Step>
             ))}
           </Stepper>
+
+          {activeSteps === steps.length ? <Confirmation /> : <Form />}
         </Paper>
       </main>
     </>
