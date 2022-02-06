c299d3d9579158a97594f5dd51d44b8ea730fca1
Yeison <enmanuelquiterios@gmail.com>
2022-02-06 13:22:45 -0500

    added btns for payemnt and back steps

--- client/src/components/Checkout/CheckoutForms/PaymentForm.js
+++ client/src/components/Checkout/CheckoutForms/PaymentForm.js
@@ -8,11 +8,42 @@ import {
 
 import { loadStripe } from "@stripe/stripe-js";
 import Review from "./Review";
+const stripePromise = loadStripe("");
 
 const PaymentForm = ({ checkoutToken, shippingData, backStep }) => {
   return (
     <>
       <Review checkoutToken={checkoutToken} />
+      <Divider />
+
+      <Typography variant="h6" gutterBottom style={{ margin: "15px 0" }}>
+        Payment Method
+      </Typography>
+      <Elements stripe={stripePromise}>
+        <ElementConsumer>
+          {({ elements, stripe }) => (
+            <form>
+              <CardElement />
+              <br />
+              <br />
+
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Button variant="outlined" onClick={backStep}>
+                  Back
+                </Button>
+                <Button
+                  variant="outlined"
+                  disabled={!stripe}
+                  color="primary"
+                  type="submit"
+                >
+                  Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                </Button>
+              </div>
+            </form>
+          )}
+        </ElementConsumer>
+      </Elements>
     </>
   );
 };
