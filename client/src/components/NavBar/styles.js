8c652899e3e5e6d7e951c1acaf90c4918c8eb844
Yeison <enmanuelquiterios@gmail.com>
2022-02-07 12:08:56 -0500

    Updating

--- client/src/components/NavBar/styles.js
+++ client/src/components/NavBar/styles.js
@@ -4,7 +4,7 @@ const drawerWidth = 0;
 
 export default makeStyles((theme) => ({
   appBar: {
-    backgroundColor: "#FFF2F9",
+    backgroundColor: "#2979ff",
     boxShadow: "none",
     borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
     [theme.breakpoints.up("sm")]: {
