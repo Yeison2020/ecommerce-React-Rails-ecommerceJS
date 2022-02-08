1edd72afb9ee6dd0ef87bb19d08e93d5f86ee01b
Yeison <enmanuelquiterios@gmail.com>
2022-02-08 12:50:54 -0500

    Adding users controller functions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,25 @@
 class Api::UsersController < ApplicationController
+    def create 
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else 
+            render json:{ errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
 
     
+
+    def show 
+        if current_user 
+            render json: current_user, statu: :ok 
+
+        else 
+            render json: 'Not authenticated', status: :unauthorized
+        end
+    end
+
   
     
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Users Routes for Creating a User and prividing users data.
+  post '/signup', to: 'users#create'
+
+  get '/me', to: 'users#show'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
